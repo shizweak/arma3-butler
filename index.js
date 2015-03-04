76515796a904ad2723a3d448a26204b15caa47ef
shizweak <gh@shizweak.es>
2015-03-05 00:10:44 +1030

    Fleshing out server model

---
+++ index.js
@@ -0,0 +1,53 @@
+var fs = require('fs');
+
+var Observify = require('observify');
+var BattleNode = require('battle-node');
+
+function serverModel(config) {
+
+	config.port = config.port || 2302;
+
+	var rcon = new BattleNode(config);
+
+	var model = Observify({
+		ip: config.ip,
+		port: config.port,
+		password: config.password,
+		players: [],
+		connected: true,
+		connecting: true,
+		tried: -1,
+	});
+
+	rcon.on('login', login);
+	rcon.on('message', message);
+
+	model.connected(connected);
+
+
+	function connect() {
+		if(model.connected())
+			return;
+
+		model.connecting.set(true);
+		rcon.login();
+	}
+
+	function login(err, success) {
+		model.connecting.set(false);
+
+	  if (err || !success)
+	  	return model.connected.set(false);
+
+  	return model.connected.set(true);
+	}
+
+	function connected(state) {
+		// when connected?
+		// get players?
+		// start schedule?
+	}
+
+
+	return model;
+}
